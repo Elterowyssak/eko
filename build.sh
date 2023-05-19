4dfc7a914d66c1d5b621bec0b1de1064ec1163aa
Elterowyssak <37578282+Elterowyssak@users.noreply.github.com>
2023-05-19 22:14:39 +0530

    uploaded run.sh and build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Change directory to your Spring Boot project directory
+cd ./eko
+
+# Run Maven build command
+mvn clean package
+
---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Change directory to your Spring Boot projt 
+cd ./eko
+
+# Run the Spring Boot application using Maven
+mvn spring-boot:run
+
